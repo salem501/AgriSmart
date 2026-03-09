64c1b5fa9bd718cd539e218f6c65adaf07c3386a
salem <salemslimi0@gmail.com>
2026-03-09 11:37:23 +0100

    replace native html tables with ag grid tables

--- src/app/pages/defis/defis.component.ts
+++ src/app/pages/defis/defis.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AccordionModule } from 'primeng/accordion';
 import { InputTextModule } from 'primeng/inputtext';
 import { TextareaModule } from 'primeng/textarea';
 import { ButtonModule } from 'primeng/button';
@@ -10,17 +9,22 @@ import { FloatLabelModule } from 'primeng/floatlabel';
 import { Defi } from '../../models/defi.model';
 import { DefiService } from '../../services/defi/defi-service';
 
+import { AgGridAngular } from 'ag-grid-angular';
+import { AllCommunityModule, ModuleRegistry, type ColDef, themeQuartz } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
 @Component({
   selector: 'app-defis',
   imports: [
     CommonModule,
     FormsModule,
-    AccordionModule,
     InputTextModule,
     TextareaModule,
     ButtonModule,
     DatePickerModule,
-    FloatLabelModule
+    FloatLabelModule,
+    AgGridAngular
   ],
   templateUrl: './defis.component.html',
   standalone: true,
@@ -32,6 +36,21 @@ export class DefisComponent implements OnInit {
   defis: Defi[] = [];
 
   showAddDefiPage = false;
+
+  theme = themeQuartz;
+
+  colDefs: ColDef[] = [
+    { field: 'titre', headerName: 'Titre', minWidth: 150 },
+    { field: 'date', headerName: 'Date', width: 150 },
+    { field: 'description', headerName: 'Description', flex: 1, minWidth: 200, wrapText: true, autoHeight: true },
+    { field: 'solutionAppliquee', headerName: 'Solution Appliquée', flex: 1, minWidth: 200, wrapText: true, autoHeight: true },
+    { field: 'resultat', headerName: 'Résultat', flex: 1, minWidth: 150, wrapText: true, autoHeight: true }
+  ];
+
+  defaultColDef = {
+    filter: true,
+    filterParams: { buttons: ['clear', 'apply'], closeOnApply: true }
+  };
 
   ngOnInit(): void {
     this.loadDefisList();
